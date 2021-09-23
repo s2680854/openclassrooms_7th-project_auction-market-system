88fe4fcb9a73008297afb72b267b65986425328e
David <d6797125@pm.me>
2021-09-23 16:52:27 +0200

    feat(model): add Rule

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rule.java
@@ -0,0 +1,11 @@
+package com.nnk.springboot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "rule")
+public class Rule {
+    // TODO: Map columns in data table RULE with corresponding java fields
+    
+}
