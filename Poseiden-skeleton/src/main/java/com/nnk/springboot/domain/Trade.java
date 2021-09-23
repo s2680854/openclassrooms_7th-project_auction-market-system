5fd35281a6b301788a3c994ea161d93108306578
David <d6797125@pm.me>
2021-09-23 16:57:23 +0200

    feat(model): update Trade

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -8,5 +8,30 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "trade")
 public class Trade {
+
     // TODO: Map columns in data table TRADE with corresponding java fields
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int tradeId;
+
+    private String account;
+    private String type;
+    private double buyQuantity;
+    private double sellQuantity;
+    private double buyPrice;
+    private double sellPrice;
+    private String benchmark;
+    private Timestamp tradeDate;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
 }
