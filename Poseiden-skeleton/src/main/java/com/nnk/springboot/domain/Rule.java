2eb8d6e035030730dda99e8be7272a0c19bd3399
David <d6797125@pm.me>
2021-09-23 16:53:52 +0200

    feat(model): update Rule

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rule.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rule.java
@@ -1,11 +1,20 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "rule")
 public class Rule {
+
     // TODO: Map columns in data table RULE with corresponding java fields
-    
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String description;
+    private String json;
+    private String template;
+    private String sqlStr;
+    private String sqlPart;
 }
