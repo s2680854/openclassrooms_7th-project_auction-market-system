40328a4d543f0d82422f880433bf1d29dab9c2df
David <d6797125@pm.me>
2021-09-23 16:51:00 +0200

    feat(model): update Rating

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -9,4 +9,12 @@ import java.sql.Timestamp;
 @Table(name = "rating")
 public class Rating {
     // TODO: Map columns in data table RATING with corresponding java fields
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String moodysRating;
+    private String sandPRating;
+    private String fitchRating;
+    private int orderNumber;
 }
