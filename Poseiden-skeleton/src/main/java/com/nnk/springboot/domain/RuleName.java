ebcbcfbb428f6f8a834fa3d58310b96550eb8787
David <d6797125@pm.me>
2021-09-24 20:14:17 +0200

    feat(model): update RuleName

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -11,4 +11,14 @@ import java.sql.Timestamp;
 @Table(name = "rulename")
 public class RuleName {
     // TODO: Map columns in data table RULENAME with corresponding java fields
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String description;
+    private String json;
+    private String template;
+    private String sqlStr;
+    private String sqlPart;
 }
