15a0b48ce25f0c8d5b0df078ca9469713d75d0d4
David <d6797125@pm.me>
2021-09-26 12:04:51 +0200

    build(model): update RuleName

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -21,4 +21,13 @@ public class RuleName {
     private String template;
     private String sqlStr;
     private String sqlPart;
+
+    public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
+        this.name = name;
+        this.description = description;
+        this.json = json;
+        this.template = template;
+        this.sqlStr = sqlStr;
+        this.sqlPart = sqlPart;
+    }
 }
