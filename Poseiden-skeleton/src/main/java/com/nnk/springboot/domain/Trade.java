80c8c7e6dea1537d21093f9213e319b0c22e1a89
David <d6797125@pm.me>
2021-09-26 12:03:45 +0200

    build(model): update Trade

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -36,4 +36,9 @@ public class Trade {
     private String dealType;
     private String sourceListId;
     private String side;
+
+    public Trade(String account, String type) {
+        this.account = account;
+        this.type = type;
+    }
 }
