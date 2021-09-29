1c03a7c738f9a891c34321f5e895752b93aad89d
David <d6797125@pm.me>
2021-09-29 16:03:18 +0200

    feat(service): update CurvePointService

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurvePointService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurvePointService.java
@@ -1,8 +1,58 @@
 package com.nnk.springboot.service;
 
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.repositories.CurvePointRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public class CurvePointService {
 
+    @Autowired
+    private CurvePointRepository curvePointRepository;
+
+    public CurvePoint getCurvePoint(Long id) {
+
+        return curvePointRepository.getById(id);
+    }
+
+    public Collection<CurvePoint> getCurvePoints() {
+
+        return curvePointRepository.findAll();
+    }
+
+    public CurvePoint creatCurvePoint(CurvePoint curvePoint) {
+
+        return curvePointRepository.save(curvePoint);
+    }
+
+    public Collection<CurvePoint> creatCurvePoints(Collection<CurvePoint> curvePoints) {
+
+        return curvePointRepository.saveAll(curvePoints);
+    }
+
+    public CurvePoint updateCurvePoint(CurvePoint curvePoint) {
+
+        Optional<CurvePoint> optionalCurvePoint =
+                curvePointRepository.findById(curvePoint.getId());
+
+        if (!optionalCurvePoint.isPresent()) {
+
+            return new CurvePoint();
+        }
+        return curvePointRepository.save(curvePoint);
+    }
+
+    public void deleteCurvePointById(Long id) {
+
+        curvePointRepository.deleteById(id);
+    }
+
+    public void deleteCurvePoints() {
+
+        curvePointRepository.deleteAll();
+    }
 }
