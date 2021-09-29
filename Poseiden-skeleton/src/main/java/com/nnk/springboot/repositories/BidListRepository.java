31a0c08dd5e824fb144189278c4fbacbb504d8ed
David <d6797125@pm.me>
2021-09-29 15:07:42 +0200

    refactor: rename models

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -1,9 +1,9 @@
 package com.nnk.springboot.repositories;
 
-import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.domain.BidsList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface BidListRepository extends JpaRepository<BidList, Integer> {
+public interface BidListRepository extends JpaRepository<BidsList, Integer> {
 
 }
