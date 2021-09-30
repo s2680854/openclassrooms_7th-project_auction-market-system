0c9680dbe7c1e175271473e375bf16a681a55722
David <d6797125@pm.me>
2021-09-30 14:26:50 +0200

    hotfix(repo): add missing anottation to RuleRepository

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleRepository.java
@@ -2,8 +2,9 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.Rule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface RuleRepository extends JpaRepository<Rule, Long> {
 
 }
