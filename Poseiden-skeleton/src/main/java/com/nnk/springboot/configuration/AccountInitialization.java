99c20a6ec6c495bb6e95c0a1deb23d3d598e5b75
David <d6797125@pm.me>
2021-09-30 16:33:49 +0200

    build(test): add post construct test user

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/AccountInitialization.java
@@ -0,0 +1,30 @@
+package com.nnk.springboot.configuration;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class AccountInitialization {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostConstruct
+    private void postConstruct() {
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setFullName("Application");
+        user.setUsername("app@test.com");
+        user.setPassword("12345678");
+        user.setRole("ADMIN");
+        userRepository.save(user);
+    }
+}
