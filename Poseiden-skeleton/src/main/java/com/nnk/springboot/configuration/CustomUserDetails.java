a97c0e66d706100f243e48aa8cf1e8da4c1d7bf7
David <d6797125@pm.me>
2021-09-30 14:49:59 +0200

    feat(security): add CustomUserDetails

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/CustomUserDetails.java
@@ -0,0 +1,12 @@
+package com.nnk.springboot.configuration;
+
+import com.nnk.springboot.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+
+}
