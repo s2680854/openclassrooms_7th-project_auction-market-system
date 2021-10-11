a9c46c4ac7c832a861d17d0fe48b975cbe7a88c2
David <d6797125@pm.me>
2021-10-11 17:42:38 +0200

    test(service): add TradeControllerTest

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/controller/UserControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controller/UserControllerTest.java
@@ -1,25 +1,18 @@
 package com.nnk.springboot.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nnk.springboot.controllers.UserController;
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
-import com.nnk.springboot.controller.user.UserCreationService;
-import com.nnk.springboot.controller.user.UserDeletionService;
-import com.nnk.springboot.controller.user.UserReadService;
-import com.nnk.springboot.controller.user.UserUpdateService;
 import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -29,16 +22,6 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @MockBean
-    private UserCreationService userCreationService;
-    @MockBean
-    private UserReadService userReadService;
-    @MockBean
-    private UserUpdateService userUpdateService;
-    @MockBean
-    private UserDeletionService userDeletionService;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -51,27 +34,56 @@ public class UserControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
     }
 
-    @BeforeEach
-    public void shouldInitialize() throws Exception {
-
-        userRepository.deleteAll();
-
-        User user = new User();
-        user.setUsername("gringotts@jkr.com");
-        user.setPassword("1234567");
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-        user.setFullname("Gringotts");
-        user.setRole("ADMIN");
-        userRepository.save(user);
-    }
-
     @Test
     public void shouldGetUserList() throws Exception {
 
         mockMvc.perform(get("/user/list"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("user/list"));
+    }
+
+    @Test
+    public void shouldAddUser() throws Exception {
+
+        mockMvc.perform(get("/user/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/add"));
+    }
+
+    @Test
+    public void shouldShowUpdateUserForm() throws Exception {
+
+        User user = new User();
+        user.setUsername("grinngotts@jkr.com");
+        user.setPassword("1234567");
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setFullname("Grinngott's");
+        user.setRole("ADMIN");
+        userRepository.save(user);
+        Long id = userRepository.findByEmail("grinngotts@jkr.com").getId();
+
+        mockMvc.perform(get("/user/update/" + id))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/update"));
+    }
+
+    @Test
+    public void shouldDeleteUser() throws Exception {
+
+        Long id = userRepository.findByEmail("david@test.com").getId();
+
+        mockMvc.perform(delete("/user/delete/" + id))
+                .andExpect(status().is(302))
+                .andExpect(view().name("user/list"));
+    }
+
+    @Test
+    public void shouldDeleteUserList() throws Exception {
+
+        mockMvc.perform(delete("/user/delete"))
+                .andExpect(status().is(302))
                 .andExpect(view().name("user/list"));
     }
 }
