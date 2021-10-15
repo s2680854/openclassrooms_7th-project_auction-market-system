c86772cbb595413d91c4cd5b3f9687d588b28057
David <d6797125@pm.me>
2021-10-15 19:51:23 +0200

    test(model): fix deleting test imports

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/TradeTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/TradeTests.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TradeTests {
