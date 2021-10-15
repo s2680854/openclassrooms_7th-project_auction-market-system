a8457b07fe2a8dd060ed0642a46161f7abc9c16f
David <d6797125@pm.me>
2021-10-15 19:25:06 +0200

    test(controller): add RuleControllerTest

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RatingRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RatingRepository.java
@@ -1,7 +1,6 @@
 package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.Rating;
-import com.nnk.springboot.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,5 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
     @Query("SELECT u FROM Rating u WHERE u.orderNumber = ?1")
-    User findByOrderNumber(Integer orderNumber);
+    Rating findByOrderNumber(Integer orderNumber);
 }
