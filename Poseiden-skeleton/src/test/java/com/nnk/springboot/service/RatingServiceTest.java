fe4bae3b166faa069f1525c42f57db816c1646cb
David <d6797125@pm.me>
2021-10-23 15:06:43 +0200

    test(service): add RatingServiceTest

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/service/RatingServiceTest.java
@@ -0,0 +1,11 @@
+package com.nnk.springboot.service;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RatingServiceTest {
+
+    
+}
