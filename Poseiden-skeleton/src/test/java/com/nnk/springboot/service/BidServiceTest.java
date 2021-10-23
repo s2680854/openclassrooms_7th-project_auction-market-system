84eb54d3658e85b276c0a54778b7ed18cf91dc28
David <d6797125@pm.me>
2021-10-23 15:20:38 +0200

    test(service): update BidServiceTest

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/service/BidServiceTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/service/BidServiceTest.java
@@ -27,5 +27,83 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @AutoConfigureMockMvc
 public class BidServiceTest {
 
-    
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private BidsListReadService bidListReadService;
+    @MockBean
+    private BidsListDeletionService bidListDeletionService;
+    @Autowired
+    private BidsListRepository bidListRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void shouldInitialize() throws Exception {
+
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setUsername("grinngotts@jkr.com");
+        user.setPassword("1234567");
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setFullname("Grinngott's");
+        user.setRole("ADMIN");
+        userRepository.save(user);
+    }
+
+    @Test
+    public void shouldGetBidsLists() throws Exception {
+
+        Collection<BidsList> actualList = new ArrayList<>();
+
+        Collection<BidsList> expectedList = bidListReadService.getBidsLists();
+
+        assertEquals(actualList, expectedList);
+    }
+
+    @Test
+    public void shouldGetBidsListById() throws Exception {
+
+        bidListRepository.deleteAll();
+        BidsList bidList = new BidsList();
+        bidList.setAccount("grinngotts@jkr.com");
+        bidList.setType("0");
+        bidList.setBidQuantity(0d);
+        bidListRepository.save(bidList);
+        Long id = 1L;
+        try {id = bidListRepository.findByAccount("grinngotts@jkr.com").getId();} catch (Exception e) {}
+
+        BidsList expected = new BidsList();
+
+        Optional<BidsList> optional = Optional.ofNullable(bidListReadService.getBidsListById(id));
+        BidsList actual = new BidsList();
+        if (optional.isPresent()) {
+            actual.setAccount(optional.get().getAccount());
+            actual.setType(optional.get().getType());
+        }
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldDeleteBidsList() throws Exception {
+
+        bidListRepository.deleteAll();
+        BidsList bidList = new BidsList();
+        bidList.setAccount("grinngotts@jkr.com");
+        bidList.setType("0");
+        bidList.setBidQuantity(0d);
+        bidListRepository.save(bidList);
+        Long bidListId = bidListRepository.findByAccount("grinngotts@jkr.com").getId();
+
+        Mockito.doNothing().when(bidListDeletionService).deleteBidsListById(bidListId);
+
+        mockMvc.perform(delete("/bidList/delete/" + bidListId));
+
+        Mockito.verify(bidListDeletionService, Mockito.times(1)).deleteBidsListById(bidListId);
+    }
 }
