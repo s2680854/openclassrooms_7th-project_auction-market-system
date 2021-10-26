566dc9f61f28138aee9a86452e5a4753d224790e
David <d6797125@pm.me>
2021-10-26 21:57:49 +0200

    fix(errors): add 403 error page

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controller/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controller/LoginController.java
@@ -3,9 +3,13 @@ package com.nnk.springboot.controller;
 import com.nnk.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("app")
@@ -30,11 +34,25 @@ public class LoginController {
     }
 
     @GetMapping("error")
-    public ModelAndView error() {
+    public ModelAndView error(HttpServletRequest request, Model model) {
         ModelAndView mav = new ModelAndView();
-        String errorMessage= "Vous n'êtes pas autorisé à accéder à ces informations.";
-        mav.addObject("errorMsg", errorMessage);
-        mav.setViewName("403");
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == 403) {
+                String errorMessage = "Vous n'êtes pas autorisé à accéder à ces informations.";
+                mav.addObject("errorMsg", errorMessage);
+                mav.setViewName("403");
+            } else if (statusCode == 404) {
+                String errorMessage = "La page est introuvable.";
+                mav.addObject("errorMsg", errorMessage);
+                mav.setViewName("404");
+            } else if (statusCode == 401) {
+                String errorMessage = "Vous n'êtes pas autorisé à accéder à ces informations.";
+                mav.addObject("errorMsg", errorMessage);
+                mav.setViewName("401");
+            }
+        }
         return mav;
     }
 }
